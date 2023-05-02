b04fd82a0e86677ea7127f6266c8d9b2446190ed
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-02 11:06:27 +0200

    refactor: define a method for update the battery charge

--- src/main/kotlin/it/nicolasfarabegoli/pulverization/OnLowBattery.kt
+++ src/main/kotlin/it/nicolasfarabegoli/pulverization/OnLowBattery.kt
@@ -4,12 +4,16 @@ import it.nicolasfarabegoli.pulverization.runtime.dsl.model.ReconfigurationEvent
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.NodeProperty
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 data class OnLowBattery(override val node: Node<Any>) : ReconfigurationEvent<Double>, NodeProperty<Any> {
-    val flow = MutableSharedFlow<Double>()
-    override val events: Flow<Double> = flow
+    private val flow = MutableStateFlow(100.0)
+    override val events: Flow<Double> = flow.asStateFlow()
     override val predicate: (Double) -> Boolean = { it < 25.0 }
+
+    fun updateBattery(newValue: Double) = flow.update { newValue }
 
     override fun cloneOnNewNode(node: Node<Any>): NodeProperty<Any> = TODO("Not yet implemented")
 }
