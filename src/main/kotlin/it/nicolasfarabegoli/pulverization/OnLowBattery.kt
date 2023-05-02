8737e99cdcb50c1b3c9f012f72ae72432a59a3b0
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-02 10:53:06 +0200

    refator: rename reconfiguration event

--- src/main/kotlin/it/nicolasfarabegoli/pulverization/OnLowBattery.kt
+++ src/main/kotlin/it/nicolasfarabegoli/pulverization/OnLowBattery.kt
@@ -6,13 +6,10 @@ import it.unibo.alchemist.model.interfaces.NodeProperty
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 
-data class ReconfigurableDevice(override val node: Node<Any>) : ReconfigurationEvent<Double>, NodeProperty<Any> {
+data class OnLowBattery(override val node: Node<Any>) : ReconfigurationEvent<Double>, NodeProperty<Any> {
     val flow = MutableSharedFlow<Double>()
-
     override val events: Flow<Double> = flow
     override val predicate: (Double) -> Boolean = { it < 25.0 }
-
-    override fun toString() = "ReconfigurableDevice[${node.id}]"
 
     override fun cloneOnNewNode(node: Node<Any>): NodeProperty<Any> = TODO("Not yet implemented")
 }
