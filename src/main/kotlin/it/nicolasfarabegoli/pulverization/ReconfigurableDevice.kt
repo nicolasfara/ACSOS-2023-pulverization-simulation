f4a82d52f3de7f82982846e15d53087287cb4650
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-02 10:11:09 +0200

    fix: solve the NPE since the class spawn also the pulverization framework not respecting the framework startup sequence

--- src/main/kotlin/it/nicolasfarabegoli/pulverization/ReconfigurableDevice.kt
+++ src/main/kotlin/it/nicolasfarabegoli/pulverization/ReconfigurableDevice.kt
@@ -1,27 +1,18 @@
 package it.nicolasfarabegoli.pulverization
 
-import it.nicolasfarabegoli.pulverization.runtime.PulverizationRuntime
 import it.nicolasfarabegoli.pulverization.runtime.dsl.model.ReconfigurationEvent
 import it.unibo.alchemist.model.interfaces.Node
 import it.unibo.alchemist.model.interfaces.NodeProperty
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.runBlocking
 
 data class ReconfigurableDevice(override val node: Node<Any>) : ReconfigurationEvent<Double>, NodeProperty<Any> {
-    private val internalFlow = MutableSharedFlow<Double>()
-    val flow get() = internalFlow.also {
-        if (!::runtime.isInitialized) {
-            runBlocking {
-                val config = configureRuntime(this@ReconfigurableDevice)
-                val runtime = PulverizationRuntime(node.id.toString(), "smartphone", config)
-                runtime.start()
-            }
-        }
-    }
+    val flow = MutableSharedFlow<Double>()
+
     override val events: Flow<Double> = flow
     override val predicate: (Double) -> Boolean = { it < 25.0 }
-    private lateinit var runtime: PulverizationRuntime<Unit, Int, Int, Unit, Unit>
+
+    override fun toString() = "ReconfigurableDevice[${node.id}]"
 
     override fun cloneOnNewNode(node: Node<Any>): NodeProperty<Any> = TODO("Not yet implemented")
 }
