63ff3789f629263dc07ef03e1a291720780d4a94
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-03 09:23:59 +0200

    refactor: remove coroutine scope and use runBlocking to prevent Alchemist start before the pulverization framework has started

--- src/main/kotlin/it/nicolasfarabegoli/pulverization/OnLowBattery.kt
+++ src/main/kotlin/it/nicolasfarabegoli/pulverization/OnLowBattery.kt
@@ -10,9 +10,9 @@ import kotlinx.coroutines.flow.asSharedFlow
 data class OnLowBattery(override val node: Node<Any>) : ReconfigurationEvent<Double>(), NodeProperty<Any> {
     private val flow = MutableSharedFlow<Double>()
     private val reconfigurationCharge by GetMolecule
+
     override val events: Flow<Double> = flow.asSharedFlow()
     override val predicate: (Double) -> Boolean = { it < node.getConcentration(reconfigurationCharge) as Double }
-
     suspend fun updateBattery(newValue: Double) = flow.emit(newValue)
 
     override fun cloneOnNewNode(node: Node<Any>): NodeProperty<Any> = TODO("Not yet implemented")
--- src/main/kotlin/it/nicolasfarabegoli/pulverization/interop/ProtelisInterop.kt
+++ src/main/kotlin/it/nicolasfarabegoli/pulverization/interop/ProtelisInterop.kt
@@ -6,15 +6,11 @@ import it.nicolasfarabegoli.pulverization.runtime.PulverizationRuntime
 import it.unibo.alchemist.model.implementations.properties.ProtelisDevice
 import it.unibo.alchemist.model.interfaces.Node.Companion.asProperty
 import it.unibo.alchemist.protelis.AlchemistExecutionContext
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.util.WeakHashMap
 
 object ProtelisInterop {
-    private val scope = CoroutineScope(Dispatchers.Unconfined)
     private val initialized: WeakHashMap<AlchemistExecutionContext<*>, Any> = WeakHashMap()
 
     @JvmStatic
@@ -22,7 +18,7 @@ object ProtelisInterop {
         val connector = (deviceUID as ProtelisDevice<*>).node.asProperty<Any, OnLowBattery>()
         runBlocking {
             connector.updateBattery(executionEnvironment.get("currentCapacity") as Double)
-            connector.results.first()
+            connector.results.first() // Needed for synchronize Alchemist with the pulverization framework
         }
     }
 
@@ -32,7 +28,7 @@ object ProtelisInterop {
             initialized[this] = true
             val deviceID = (deviceUID as ProtelisDevice<*>)
             val reconfigurationEvent = deviceID.node.asProperty<Any, OnLowBattery>()
-            scope.launch {
+            runBlocking {
                 val config = configureRuntime(reconfigurationEvent)
                 val runtime = PulverizationRuntime(deviceID.id.toString(), "smartphone", config)
                 runtime.start()
